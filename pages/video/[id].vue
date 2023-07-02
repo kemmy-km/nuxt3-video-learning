c2de67ff461c5d2e2abdedf077fd92b914ff11c4
kemmy-km <changem2mind@gmail.com>
2023-07-02 12:39:02 +0900

    前へ、次へボタン遷移の実装

--- pages/video/[id].vue
+++ pages/video/[id].vue
@@ -1,26 +1,25 @@
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 import { useRouter } from 'vue-router'
 import { API_BASE_URL } from '~/constants/common'
 
 const router = useRouter()
-const videoCode = ref('')
+const currentVideoCode = ref(0)
 const videoDomain = ref("https://player.vimeo.com")
 
 /** ここに$fetch()した結果を代入させる */
 const video = ref()
 
 /** コースに所属している動画群 */
-const courseVideos = ref([])
+let courseVideos = ref([])
 
 onMounted(async () => {
 
   // ルートパラメータから`videoCode`の値を取得
-  videoCode.value = router.currentRoute.value.params.id
-  console.log("videoCode:", videoCode.value)
+  currentVideoCode.value = Number(router.currentRoute.value.params.id)
 
   try {
-    const response = await $fetch(`${API_BASE_URL}/video/${videoCode.value}`)
+    const response = await $fetch(`${API_BASE_URL}/video/${currentVideoCode.value}`)
     if (!!response) {
       console.log('動画詳細ページのresponse！')
 
@@ -29,7 +28,6 @@ onMounted(async () => {
 
       /** コース動画プレイヤー一覧 */
       const response2 = await $fetch(`${API_BASE_URL}/course/videos/${video.value.courseId}`)
-      if (!!response2) return
 
       // 取得したデータを`courseVideos`に設定
       courseVideos.value = response2
@@ -39,6 +37,60 @@ onMounted(async () => {
     console.log('失敗！')
     console.error(error)
   }
+})
+
+/** そのコースに所属する「前」or「次」の動画に遷移 */
+const showFrontOrRearVideo = (isToFront) => {
+  let code = ""
+
+  if (isToFront) {
+    // 「前へ」ボタン
+
+    // 降順にソートする
+    courseVideos.value = courseVideos.value.sort((a, b) => b.videoCode - a.videoCode);
+
+    for (let courseVideo of courseVideos.value) {
+      if (courseVideo.videoCode < currentVideoCode.value) {
+
+        // `courseVideo.videoCode`を「前ボタン」で使う変数に代入
+        code = courseVideo.videoCode
+        break
+      }
+    }
+
+  } else {
+    // 「次へ」ボタン
+
+    for (let courseVideo of courseVideos.value) {
+      if (courseVideo.videoCode > currentVideoCode.value) {
+
+        // `courseVideo.videoCode`を「次ボタン」で使う変数に代入
+        code = courseVideo.videoCode
+        break
+      }
+    }
+  }
+
+  router.push(`/video/${code}`)
+}
+
+/** コース内の動画で、最小のvideoCodeを取得する */
+const minVideoCode = computed(() => {
+  return Math.min(...courseVideos.value.map(video => video.videoCode))
+})
+
+/** コース内の動画で、最大のvideoCodeを取得する */
+const maxVideoCode = computed(() => {
+  return Math.max(...courseVideos.value.map(video => video.videoCode))
+})
+
+// currentVideoCodeとminVideoCodeを比較し、最小のvideoCodeを持つ動画かどうかを判定する
+const isMinVideoCode = computed(() => {
+  return currentVideoCode.value === minVideoCode.value
+})
+
+const isMaxVideoCode = computed(() => {
+  return currentVideoCode.value === maxVideoCode.value
 })
 
 // コンポーネントツリーに提供する
@@ -69,9 +121,18 @@ provide('courseVideos', courseVideos)
           <p>概要：</p>
         </div>
 
-        <div class="buttonWrapper">
-          <button class="commonButton">＜ 前の動画へ</button>
-          <button class="commonButton">次の動画へ ＞</button>
+        <div class="buttonWrapper flex mb-1">
+          <button
+            class="commonButton"
+            v-if="!isMinVideoCode"
+            @click="showFrontOrRearVideo(true)"
+          >＜ 前の動画へ</button>
+          <button
+            class="commonButton"
+            v-if="!isMaxVideoCode"
+            @click="showFrontOrRearVideo(false)"
+          >次の動画へ ＞</button>
+
         </div>
 
         <div class="buttonWrapper">
@@ -85,7 +146,8 @@ provide('courseVideos', courseVideos)
     <div class="col block__videoPlayerList">
 
       <div class="row playerList__heading">
-        <h3 class="heading__lv3 mb-2">コースの内容</h3>
+        <!-- ここに、コースの名前を表示させたい -->
+        <h3 class="heading__lv3 mb-2">コースの内容：{{ video?.courseId }}</h3>
         <button class="btn btn-info for-sp btn-toggle commonButton">プレイヤーリストを開く</button>
       </div>
 
--- types/api/video.ts
+++ types/api/video.ts
@@ -1,7 +1,7 @@
 /** 動画一覧ページリクエスト */
 export type VideoRequest = {
   /** 動画コード */
-  code: string
+  videoCode: number
   /** 所属するコース */
   courseId: string
 }
@@ -9,7 +9,9 @@ export type VideoRequest = {
 /** 動画一覧ページレスポンス */
 export type VideoResponse = {
   /** 動画コード */
-  code: string
+  videoCode: number
+  /** コースID */
+  courseId: string
   /** vimeoID */
   videoNumber: string
   /** 動画タイトル */
@@ -21,17 +23,30 @@ export type VideoResponse = {
 /** 動画詳細ページリクエスト */
 export type VideoDetailRequest = {
   /** 動画コード */
-  code: string
+  videoCode: number
   /** 所属するコース */
   courseId: string
   // これは、コースの一覧画面から特定の動画を視聴したい際
 }
 // それともコース単位？コース＝動画のひとかたまり、とも言える
 
+/** コースに所属する動画Listの取得Request */
+export type VideoByCourseIdRequest = {
+  /** コースID */
+  courseId: string
+}
+
+/** コースに所属する動画Listの取得Response */
+export type VideoByCourseIdResponse = {
+  videoList: VideoDetailRequest[]
+}
+
 /** 動画詳細ページレスポンス */
 export type VideoDetailResponse = {
   /** 動画コード */
-  code: string
+  videoCode: number
+  /** コースID */
+  courseId: string
   /** vimeoID */
   videoNumber: string
   /** 動画タイトル */
