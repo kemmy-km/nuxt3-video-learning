b062307a589ef31e950b3a9ea4a59275b3764f31
kemmy-km <changem2mind@gmail.com>
2023-07-24 21:23:18 +0900

    APIのURL修正

--- constants/common.ts
+++ constants/common.ts
@@ -1,4 +1,5 @@
-export const API_BASE_URL: string = "http://127.0.0.1:8000/api"
+// export const API_BASE_URL: string = "http://127.0.0.1:8000/api"
+export const API_BASE_URL: string = "http://kemmy.tokyo/api"
 
 /** vimeo */
 export const VIDEO_DOMAIN_URL: string = "https://player.vimeo.com"
